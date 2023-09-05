4df867491e91335c4a5dc60db90add58a180584a
SunTeco <sunteco21@gmail.com>
2023-09-05 17:25:44 -0400

    loadcsv delay

--- src/component/Dashboard/Dashboard.jsx
+++ src/component/Dashboard/Dashboard.jsx
@@ -32,24 +32,27 @@ const Dashboard = () => {
       async function getData() {
         const company_data = await getLocalCsvData("/data/company_data.csv")
         const rows = await getLocalCsvData("/data/data.csv")
-        var currentdate = new Date(); 
-        // Upcoming records filter
-        var upcomingCsvData = rows.filter((row)=>{
-          // console.log(row)
-          if(!!!row.distribution_date) return false;
-          var date_parts = row.distribution_date.split('/')
-          var d = new Date(+date_parts[2], date_parts[1] - 1, +date_parts[0]);
-          return d.getTime() > currentdate.getTime();
-        })
-        // console.log(company_data)
-        for (var i = 0; i < upcomingCsvData.length; i++) {
-          const symbol = upcomingCsvData[i].symbol
-          const company_info = company_data.find(company => company.companySymbol === symbol)
-          console.log(symbol, company_info)
-          upcomingCsvData[i].company_url = company_info.companyUrl;
-        }
-        setUpcomingData(upcomingCsvData);
-        setParsedCsvData(rows);
+        const timer = setTimeout(() => {
+          var currentdate = new Date(); 
+          // Upcoming records filter
+          var upcomingCsvData = rows.filter((row)=>{
+            // console.log(row)
+            if(!!!row.distribution_date) return false;
+            var date_parts = row.distribution_date.split('/')
+            var d = new Date(+date_parts[2], date_parts[1] - 1, +date_parts[0]);
+            return d.getTime() > currentdate.getTime();
+          })
+          // console.log(company_data)
+          for (var i = 0; i < upcomingCsvData.length; i++) {
+            const symbol = upcomingCsvData[i].symbol
+            const company_info = company_data.find(company => company.companySymbol === symbol)
+            console.log(symbol, company_info)
+            upcomingCsvData[i].company_url = company_info.companyUrl;
+          }
+          setUpcomingData(upcomingCsvData);
+          setParsedCsvData(rows);
+          }, 1000);
+        return () => clearTimeout(timer);         
       }
       getData();
   }, []);
